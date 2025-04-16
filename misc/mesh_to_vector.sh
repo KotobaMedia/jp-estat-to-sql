9811337e3a3e63cc8c348507cd0a04f041a301b9
Keita Kobayashi <keita@kbys.me>
2025-04-16 17:35:40 +0900

    Provisional 1-2 mesh layers

--- misc/mesh_to_vector.sh
+++ misc/mesh_to_vector.sh
@@ -14,38 +14,61 @@ trap 'rm -rf "$tmpdir"' EXIT
 output_file=$1
 pg_connection_string=$2
 
+ogr2ogr -f FlatGeobuf "$tmpdir/output1.fgb" PG:"$pg_connection_string" -sql 'SELECT left(x."KEY_CODE"::text, 4)::bigint as "KEY_CODE", sum(x."人口（総数）") as "人口（総数）", sum(x."人口（総数）男") as "人口（総数）男", sum(x."人口（総数）女") as "人口（総数）女", jismesh.to_meshpoly_geom(left(x."KEY_CODE"::text, 4)::bigint) as "geom" FROM jp_estat_mesh_2020_t001140_3 x GROUP BY left(x."KEY_CODE"::text, 4)::bigint'
+
+ogr2ogr -f FlatGeobuf "$tmpdir/output2.fgb" PG:"$pg_connection_string" -sql 'SELECT left(x."KEY_CODE"::text, 6)::bigint as "KEY_CODE", sum(x."人口（総数）") as "人口（総数）", sum(x."人口（総数）男") as "人口（総数）男", sum(x."人口（総数）女") as "人口（総数）女", jismesh.to_meshpoly_geom(left(x."KEY_CODE"::text, 6)::bigint) as "geom" FROM jp_estat_mesh_2020_t001140_3 x
+GROUP BY left(x."KEY_CODE"::text, 6)::bigint'
+
 ogr2ogr -f FlatGeobuf "$tmpdir/output3.fgb" PG:"$pg_connection_string" -mapFieldType IntegerList=String,Integer64List=String -sql "SELECT x.*, jismesh.to_meshpoly_geom(x.\"KEY_CODE\") as \"geom\" FROM jp_estat_mesh_2020_t001140_3 x"
 
 ogr2ogr -f FlatGeobuf "$tmpdir/output4.fgb" PG:"$pg_connection_string" -mapFieldType IntegerList=String,Integer64List=String -sql "SELECT x.*, jismesh.to_meshpoly_geom(x.\"KEY_CODE\") as \"geom\" FROM jp_estat_mesh_2020_t001141_4 x"
 
 ogr2ogr -f FlatGeobuf "$tmpdir/output5.fgb" PG:"$pg_connection_string" -mapFieldType IntegerList=String,Integer64List=String -sql "SELECT x.*, jismesh.to_meshpoly_geom(x.\"KEY_CODE\") as \"geom\" FROM jp_estat_mesh_2020_t001142_5 x"
 
+tippecanoe -o "$tmpdir/1.mbtiles" \
+    -Z0 \
+    -z4 \
+    --no-simplification-of-shared-nodes \
+    -f \
+    --layer=jp_estat_mesh_2020 \
+    "$tmpdir/output1.fgb"
+
+tippecanoe -o "$tmpdir/2.mbtiles" \
+    -Z5 \
+    -z8 \
+    --no-simplification-of-shared-nodes \
+    -f \
+    --layer=jp_estat_mesh_2020 \
+    "$tmpdir/output2.fgb"
+
 tippecanoe -o "$tmpdir/3.mbtiles" \
-  -Z0 \
-  -z9 \
-  --no-simplification-of-shared-nodes \
-  -f \
-  --layer=jp_estat_mesh_2020 \
-  "$tmpdir/output3.fgb"
+    -Z9 \
+    -z10 \
+    --no-simplification-of-shared-nodes \
+    -f \
+    --layer=jp_estat_mesh_2020 \
+    "$tmpdir/output3.fgb"
 
 tippecanoe -o "$tmpdir/4.mbtiles" \
-  -Z10 \
-  -z12 \
-  --no-simplification-of-shared-nodes \
-  -f \
-  --layer=jp_estat_mesh_2020 \
-  "$tmpdir/output4.fgb"
+    -Z11 \
+    -z11 \
+    --no-simplification-of-shared-nodes \
+    -f \
+    --layer=jp_estat_mesh_2020 \
+    "$tmpdir/output4.fgb"
 
 tippecanoe -o "$tmpdir/5.mbtiles" \
-  -Z13 \
-  -z13 \
-  --no-simplification-of-shared-nodes \
-  -f \
-  --layer=jp_estat_mesh_2020 \
-  "$tmpdir/output5.fgb"
+    -Z12 \
+    -z12 \
+    --no-simplification-of-shared-nodes \
+    -f \
+    --layer=jp_estat_mesh_2020 \
+    "$tmpdir/output5.fgb"
 
 tile-join -o "$output_file" \
-  --force \
-  "$tmpdir/3.mbtiles" \
-  "$tmpdir/4.mbtiles" \
-  "$tmpdir/5.mbtiles"
+    --force \
+    "$tmpdir/1.mbtiles" \
+    "$tmpdir/2.mbtiles" \
+    "$tmpdir/3.mbtiles" \
+    "$tmpdir/4.mbtiles" \
+    "$tmpdir/5.mbtiles"
